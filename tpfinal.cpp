e1dbb6c8c7a5158eb449b3d16fc88115ed88dc7e
Santiago Mael Pandelo <spandelo@frba.utn.edu.ar>
2024-10-08 19:37:54 -0300

    Push tpfinal.cpp
    Commit para agregar la primera version del tpfinal

---
+++ tpfinal.cpp
@@ -0,0 +1,53 @@
+#include <iostream>;
+#include <cstring>;
+using namespace std;
+
+struct pacientes{
+    int id;
+    char nom[51];
+    char ape[51];
+    int edad;
+    int dni;
+    int telefono;
+
+};
+
+struct nodo{
+    turnos info;
+    nodo*sgte;
+};
+
+struct dia{
+    bool atiende;
+    nodo*lista;
+};
+
+struct turnos{
+    int idunico;
+    int hora;
+    int dia;
+    int mes;
+    char status[2];
+    int idpacientes;
+};
+
+struct medico{
+    int id;
+    char nom[51];
+    char ape[51];
+    int matricula;
+    int idespe;
+    dia atencion[7];
+    int rangohor[2];
+    int tiempocon;
+
+};
+
+
+int main(){
+    
+    char espe[20][51];
+
+
+    return 0;
+}
